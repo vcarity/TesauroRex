9a10444da8627b784f9eab9428d09c3650a74221
b1ck.w0lf <victorcfsc@dcomp.ufs.br>
2018-12-04 01:06:33 -0200

    Teste com CalculadoraLex
    Realizei os testes utilizando o exemplo disponibilizado.

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,30 @@
-/* Create an AST, then invoke our interpreter. */ 
-import tesauroRex.interpret.Interpreter;
-import tesauroRex.parser.* ;
-import tesauroRex.lexer.* ;
-import tesauroRex.node.* ;
- 
-import java.io.* ;
- 
+import tesauroRex.lexer.*;
+import tesauroRex.node.*;
+import java.io.*;
+
 public class Main {
-   public static void main(String[] args) {
-      if (args.length > 0) {
-         try {
-            /* Form our AST */
-            Lexer lexer = new Lexer (new PushbackReader(
-               new FileReader(args[0]), 1024));
-            Parser parser = new Parser(lexer);
-            Start ast = parser.parse() ;
- 
-            /* Get our Interpreter going. */
-            Interpreter interp = new Interpreter () ;
-            ast.apply(interp) ;
-         }
-         catch (Exception e) {
-            System.out.println (e) ;
-         }
-      } else {
-         System.err.println("usage: java tesauroRex inputFile");
-         System.exit(1);
-      }
-   }
-} 
+	
+  public static void main(String[] args) {
+	  
+    try {
+			
+      String arquivo = "test/input.test";
+	  Lexer lexer = new Lexer(new PushbackReader(new FileReader(arquivo), 1024)); 
+	  Token token;
+	  
+      while(!((token = lexer.next()) instanceof EOF)) {
+    	  
+        System.out.print(token.getClass());
+        System.out.println(" ( "+token.toString()+")");
+        
+	  }
+      
+	} catch(Exception e) {
+		
+      System.out.println(e.getMessage());
+      
+    }
+    
+  }
+  
+}
