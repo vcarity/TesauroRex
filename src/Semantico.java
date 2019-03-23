52d42ac7dd06d7c82e55653a8e84c4f1b0dc8d45
Victor <victorcfsc@dcomp.ufs.br>
2019-03-22 23:41:48 -0300

    Semantico
    Trabalhando nas atribuições.

--- src/Semantico.java
+++ src/Semantico.java
@@ -11,6 +11,26 @@ public class Semantico extends DepthFirstAdapter {
 	public Stack getPilha() {
 		return this.pilha;
 	}
+	
+	public Boolean conferirTipo(String tipo, String valor) {
+		switch (tipo) {
+		case "integer ":
+			//if (valor.charAt(1) == 'd' 
+			//	|| valor.charAt(1) == 'b') 
+			//{
+			//	return true;
+			//} else {
+			//	return false;
+			//}
+			return true;
+		case "real ":
+			return true;
+		case "symbol ":
+			return true;
+		default :
+			return false;
+		}
+	}
 
 	@Override
 	public void inStart(Start node)
@@ -33,11 +53,9 @@ public class Semantico extends DepthFirstAdapter {
 	 {
 		 if (pilha.empty()) {
 			 Hashtable tabelaDeSimbolos = new Hashtable();
-			 Simbolo simbolo = new Simbolo(node.getId().toString(), node.getClass().getSimpleName().toString());
+			 Simbolo simbolo = new Simbolo(node.getId().toString(), "Programa");
 			 tabelaDeSimbolos.put(simbolo.getId(), simbolo);
 			 pilha.push(tabelaDeSimbolos);
-			 //System.out.println(tabelaDeSimbolos);
-			 //System.out.println(tabelaDeSimbolos.get("Programa"));
 		 } else {
 			 System.out.println("-------------------------------------------------");
 			 System.out.println("Erro: Programa já declarado.");
@@ -73,11 +91,68 @@ public class Semantico extends DepthFirstAdapter {
 	 @Override
 	 public void inAVariableDeclaracao(AVariableDeclaracao node)
 	 {
-		 Simbolo simbolo = new Simbolo(node.getIdentifier().toString(), node.getClass().getSimpleName().toString());
 		 Hashtable tabelaDeSimbolos = (Hashtable) getPilha().peek();
-		 tabelaDeSimbolos.put(simbolo.getId(), simbolo);
-		 //System.out.println(getPilha().peek());
-		 //System.exit(0);
+		 String variavel = node.getIdentifier().toString();
+		 Simbolo simboloVariavel = (Simbolo) tabelaDeSimbolos.get(variavel);
+		 if (simboloVariavel == null) {
+			 Simbolo simbolo = new Simbolo(node.getIdentifier().toString(), node.getTipo().toString());
+			 tabelaDeSimbolos.put(simbolo.getId(), simbolo);
+		 } else {
+			 System.out.println("-------------------------------------------------");
+			 System.out.println("Erro: variável " + variavel + " já existe.");
+			 System.out.println("Análise Semântica Encerrada.");
+			 System.out.println("-------------------------------------------------");
+			 System.exit(0);
+		 }
+	 }
+	 
+	 @Override
+	 public void outAVariableDeclaracao(AVariableDeclaracao node)
+	 {
+		 //
+	 }
+	 
+	 @Override
+	 public void inAConstDeclaracao(AConstDeclaracao node)
+	 {
+		 Hashtable tabelaDeSimbolos = (Hashtable) getPilha().peek();
+		 String variavel = node.getIdentifier().toString();
+		 Simbolo simboloVariavel = (Simbolo) tabelaDeSimbolos.get(variavel);
+		 if (simboloVariavel == null) {
+			 Simbolo simbolo = new Simbolo(node.getIdentifier().toString(), node.getTipo().toString());
+			 tabelaDeSimbolos.put(simbolo.getId(), simbolo);
+		 } else {
+			 System.out.println("-------------------------------------------------");
+			 System.out.println("Erro: variável " + variavel + " já existe.");
+			 System.out.println("Análise Semântica Encerrada.");
+			 System.out.println("-------------------------------------------------");
+			 System.exit(0);
+		 }
+	 }
+	 
+	 @Override
+	 public void outAConstDeclaracao(AConstDeclaracao node)
+	 {
+		 //
+	 }
+	 
+	 @Override
+	 public void inAAtribuicaoComando(AAtribuicaoComando node)
+	 {
+		 Hashtable tabelaDeSimbolos = (Hashtable) getPilha().peek();
+		 String variavel = "["+ node.getVariavel().toString() + "]";
+		 Simbolo simboloVariavel = (Simbolo) tabelaDeSimbolos.get(variavel);
+		 if (conferirTipo(simboloVariavel.getTipo(), node.getExpr().toString())) {
+			 Simbolo simbolo = new Simbolo(node.getExpr().toString(), node.getVariavel().toString());
+			 tabelaDeSimbolos.put(simbolo.getId(), simbolo);
+		 } else {
+			 System.out.println("-------------------------------------------------");
+			 System.out.println("Erro: Atribuição incorreta");
+			 System.out.println(node.getExpr().toString() + " não é do tipo " + simboloVariavel.getTipo());
+			 System.out.println("Análise Semântica Encerrada.");
+			 System.out.println("-------------------------------------------------");
+			 System.exit(0);
+		 }
 	 }
 
 }
